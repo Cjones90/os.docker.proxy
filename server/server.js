9ee0365e1c13adf2519fe30a2381c256ce584098
KC <kcjones.2002@gmail.com>
2018-03-05 12:52:58 -0800

    Update npm and to 8.9 node Use alpine for smaller image footprint Use multi-stage builds in Dockerfile for far superior images Split docker-compose files Update to latest docker-compose/engine version Add graceful shutdown to server Add intelligent caching to files! Move pm2.config to server dir Add a default 404 page  - Maybe add in the console more info for debugging (within reason)

--- server/server.js
+++ server/server.js
@@ -41,11 +41,32 @@ const onHttpsUpgrade = (req, socket, head) => {
     });
 }
 
+function send404(req, res) {
+    // TODO: Seperate 404's depending on what we found
+    let extname = path.extname(url.parse(req.url).pathname);
+    let file = (url.parse(req.url).pathname).slice(1);
+    let contentTypes = {
+        ".js": "text/javascript",
+        ".ico": "text/x-icon",
+        ".html": "text/html",
+    }
+    let filePath = contentTypes[extname] ? PUB_FILES+file : PUB_FILES+"index.html"
+    let contentType = contentTypes[extname] ? contentTypes[extname] : 'text/html';
+    res.setHeader('Cache-Control', 'public, max-age=' + (1000 * 60 * 60 * 24 * 30))
+    res.writeHead(200, {"Content-Type": contentType});
+    fs.readFile(filePath, "utf8", (err, data) => {
+        if(filePath.match(`${PUB_FILES}index.html`)) {
+            data = data.replace(/%%VERSION%%/, service.IMAGE_VER)
+        }
+        res.end(data)
+    })
+}
+
 // Servers
 const httpServer = http.createServer((req, res) => {
     if(!HOSTS[req.headers.host]) {
         console.log(`Could not find 'req.headers.host': ${req.headers.host} in 'HOSTS'`);
-        return res.end("404 - Invalid host");
+        return send404(req, res);
     }
 
     let requrl = url.parse(req.url).pathname
@@ -67,10 +88,11 @@ const httpServer = http.createServer((req, res) => {
     else {
         proxy.web(req, res, { target: "http://"+HOSTS[req.headers.host] }, (err) => {
             console.log("ERR - SERVER.HTTP_SERVER:\n", err);
-            res.end("404 - Host appears to be down.")
+            return send404(req, res);
         })
     }
 })
+registerGracefulShutdown(httpServer)
 httpServer.on("upgrade", onHttpUpgrade)
 httpServer.listen(HTTP_CONTAINER_PORT, console.log("HTTP proxy on port: "+HTTP_PORT));
 if(REGISTER_SERVICE) { service.register(); }
@@ -97,9 +119,25 @@ if(SSL_PROXY_ON) {
         if(!HOSTS[req.headers.host]) { return res.end("404 - Invalid host"); }
         proxy.web(req, res, { target: "https://"+HOSTS[req.headers.host] }, (err) => {
             console.log("ERR - SERVER.HTTPS_SERVER:\n", err);
-            res.end("404 - Host appears to be down.")
+            return send404(req, res);
         })
     })
+    registerGracefulShutdown(httpsServer)
     httpsServer.on("upgrade", onHttpsUpgrade)
     httpsServer.listen(HTTPS_CONTAINER_PORT, console.log("HTTPS proxy on port: "+HTTPS_PORT));
+}
+
+function registerGracefulShutdown(server) {
+    let close = () => {
+        console.log("Received SIG signal, shutting down");
+        server.close(() => {
+            console.log("Closed out all connections successfully");
+            process.exit();
+        })
+    }
+    process.on("SIGTERM", close)
+    process.on("SIGHUP", close)
+    process.on("SIGINT", close)
+    process.on("SIGQUIT", close)
+    process.on("SIGABRT", close)
 }
--- src/jsx/Entry.jsx
+++ src/jsx/Entry.jsx
@@ -5,23 +5,97 @@ import DOM from 'react-dom';
 
 require("../style/Entry.less")
 
+// TODO: After a few entries see if we can get a 200 code from the intended
+//   url. If we can, inform and redirect while they didn't need to do anything.
+//   Otherwise continue and let them know to try again soon
+const story = ["Hey", "It looks like you need some help.", "Let's see here...",
+    "Nope... it's not that.", "Hmm", "I'm sorry but I don't think it's there.",
+    "Hopefully nothing bad happened to it..", "Maybe try again in a minute?"];
+
 const Entry = React.createClass({
 
+    // NOTE: Localstorage hasvisted thing kinda busted a bit but its not that important
+    //  so we're not gonna worry about making sure its 100%
     getInitialState() {
+        let hasVisted = localStorage.getItem("hasVisted")
+        //TODO: Set an expiry period for refreshcount
+        let refreshCount = localStorage.getItem("refreshCount") || -1
+        localStorage.setItem("refreshCount", ++refreshCount)
+        hasVisted = hasVisted ? JSON.parse(hasVisted) : false
         return {
+            string: hasVisted ? story : [],
+            fadeIn: hasVisted ? 1 : 0,
+            hasVisted: hasVisted ? true : false,
+            msgDone: hasVisted ? true : false,
+            refreshCount: refreshCount || 0
         };
     },
 
-    componentDidMount() {
-
+    appendString(string) {
+        this.state.string.push(string)
+        this.setState({ string: this.state.string })
     },
 
+    componentDidMount() {
+        let timer = 0;
+        let prevStrLen = 0
+        !this.state.hasVisted && story.forEach((str, ind) => {
+            let appendMsgIn = (prevStrLen >= 15 ? 1000 : 0) + (1500 * ind)
+            // let appendMsgIn = 2
+            prevStrLen = str.length
+            setTimeout(() => {
+                this.appendString(str)
+                if(ind === story.length-1) { this.msgIsDone() }
+            }, appendMsgIn)
+        })
+    },
+
+    msgIsDone() {
+        setTimeout(() => this.setState({ msgDone: true }, this.startFadein), 3000)
+    },
+
+    startFadein() {
+        this.interval = setInterval(() => {
+            if(this.state.fadeIn <= 1) {
+                this.setState({
+                    fadeIn: this.state.fadeIn + .010
+                })
+            }
+            else {
+                clearInterval(this.interval)
+            }
+        }, 20)
+    },
+
+    setHasVisited() {
+        localStorage.setItem("hasVisted", "true")
+        location.reload()
+    },
 
     render() {
 
-        return (
-            <div>
+        let msg = this.state.string.map((str, ind) =>
+            <span key={ind}>{str}<br /></span>
+        )
 
+        let refreshMsg = this.state.fadeIn >= .9
+            ? <div id={"refresh"}>(You can click the page to refresh)</div>
+            : null
+
+        let refreshCount = this.state.fadeIn >= .9
+            ? <div id={"refresh"}>Refresh count: {this.state.refreshCount}</div>
+            : null
+
+        return (
+            <div onClick={this.setHasVisited}>
+                {msg}
+                <div id={"span404"} style={{
+                    opacity: `${this.state.msgDone?this.state.fadeIn:0}`
+                    }}>
+                    404
+                </div>
+                {refreshMsg}
+                {refreshCount}
             </div>
         );
     }
